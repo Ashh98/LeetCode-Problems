b32329d3172604567e890f73d52105fe3fc0b415
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2022-11-09 00:10:15 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (69.85%) - LeetHub

---
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> pathInZigZagTree(int label) {
+        if (label == 1) return {1};
+        
+        int low = 2; int high = 2 * 2; int h = 2;
+        int ldiff = 0; int hdiff = 0;
+        
+        while (!(label >= low && label <= high-1)) {
+            low *= 2; high *= 2; h++;
+        }
+        vector<int> path(h, 0);
+        path[0] = 1; path[h-1] = label;
+        
+        label = label/2;
+        low = low/2; high = high/2;
+        h = h-1;
+        while (label > 1) {          
+            cout << low << " " << high-1 << " " << label << endl;
+            ldiff = abs(low - label); hdiff = abs(high - 1 - label);
+            cout << ldiff << " " << hdiff << " " << h << endl;
+            if (ldiff < hdiff) path[h-1] = high-ldiff-1;
+            else path[h-1] = hdiff + low;
+            cout << path[h-1] << endl;
+            label = path[h-1]/2; low = low/2; high = high/2; h = h-1;
+        }
+        return path;
+    }
+};
