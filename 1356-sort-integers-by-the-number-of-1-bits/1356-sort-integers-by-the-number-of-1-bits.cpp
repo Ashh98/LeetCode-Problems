080697aa5a36cae966cef4eec8768f5d40d3fcfa
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2022-11-20 08:25:19 +0200

    Time: 15 ms (75.84%), Space: 11 MB (15.78%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    int countOnes(int x) {
+        int c = 0;
+        while (x) {
+            if (x%2 == 1) c++;
+            x >>= 1;
+        }
+        return c;
+    }
+    
+    void insertionSort(vector<int>& arr) {
+        int hole;
+        for (int i = 0; i < arr.size(); i++) {
+            hole = i;
+            while (hole > 0 && arr[hole-1] > arr[hole]) {
+                swap(arr[hole-1], arr[hole]);
+                hole--;
+            }
+        }
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) {
+        vector<int> arrOnes(arr.size(), 0);
+        int max = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            arrOnes[i] = countOnes(arr[i]);
+            if (arrOnes[i] > max) max = arrOnes[i];
+        }
+
+        vector<vector<int>> x(max+1, {0});
+        for (auto& i : x) {
+            i.pop_back();
+        }
+        
+        for (int i = 0; i < arr.size(); i++) {
+            x[arrOnes[i]].push_back(arr[i]);
+        }
+                
+        vector<int> res;
+        for (auto& i : x) {
+            insertionSort(i);
+            for (auto& j : i) {
+                res.push_back(j);
+            }
+        }
+        
+        return res;
+        
+    }
+};
