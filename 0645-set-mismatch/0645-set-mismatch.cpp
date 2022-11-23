00d6166392e51b2bb4bb6eae59967959d2631727
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2022-11-23 17:08:08 +0200

    Time: 43 ms (85.30%), Space: 21.5 MB (71.00%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -14,9 +14,7 @@ public:
             if (nums[abs(i)-1] < 0) {
                 res[0] = abs(i);
                 break;
-            } else {
-                nums[abs(i)-1] = -nums[abs(i)-1];
-            }
+            } else nums[abs(i)-1] = -nums[abs(i)-1];
         }
         res[1] = (n*(n+1)/2) - (sum - res[0]);
         return res;
