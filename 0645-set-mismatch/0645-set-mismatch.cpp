a1226a065c5eb54055a1089c3413740aec3f2550
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2022-11-23 17:05:52 +0200

    Time: 43 ms (85.30%), Space: 21.3 MB (88.58%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sumVec(vector<int>& vec) {
+        int sum = 0;
+        for (auto& i : vec) {
+            sum += i;
+        }
+        return sum;
+    }
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> res = {0, 0}; int n = nums.size();
+        int sum = sumVec(nums);
+        for (auto& i : nums) {
+            if (nums[abs(i)-1] < 0) {
+                res[0] = abs(i);
+                break;
+            } else {
+                nums[abs(i)-1] = -nums[abs(i)-1];
+            }
+        }
+        res[1] = (n*(n+1)/2) - (sum - res[0]);
+        return res;
+    }
+};
