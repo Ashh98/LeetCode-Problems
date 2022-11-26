a6c3c3d557866c8dcd434eb6ee0adaf31538ac2f
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2022-11-27 00:49:46 +0200

    Time: 20 ms (25.76%), Space: 9.4 MB (90.24%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<string> letterCasePermutation(string s) {
-        int c = 0; int tmp = 0;
+        int c = 0; int tmp = 0; string t;
         vector<string> res;
         for (auto& i : s) {
             if (isalpha(i)) c++;
         }
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
         for (int i = 0; i < pow(2, c); i++) {
-            transform(s.begin(), s.end(), s.begin(), ::tolower);
+            t = s;
             int ind = 0;
             tmp = i;
             while (tmp) {
-                if (isalpha(s[ind])) {
-                    if (tmp%2) s[ind] = toupper(s[ind]);
+                if (isalpha(t[ind])) {
+                    if (tmp%2) t[ind] = toupper(t[ind]);
                     tmp >>= 1;
                 }
                 ind++;
             }
-            res.push_back(s);
+            res.push_back(t);
         } 
         return res;
     }
