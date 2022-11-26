30b453645fc90e4c66cf087874a01ae7935e3159
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2022-11-26 18:30:22 +0200

    Time: 7 ms (60.08%), Space: 7.4 MB (83.99%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int getXOR(vector<int>& arr, int f, int l) {
+    int getXOR(vector<int>& arr) {
         int xor_res = 0;
-        for (int i = f; i <= l; i++) {
+        for (int i = 0; i < arr.size(); i++) {
             xor_res ^= arr[i];
         }
         return xor_res;
     }
     int countTriplets(vector<int>& arr) {
-        int c = 0, a = 0, b = 0;
-        for (int i = 1; i < arr.size(); i++) {
-            for (int j = 0; j < arr.size()-i; j++) {
-                if (getXOR(arr, j, j+i) == 0) c += i;
+        int c = 0, a = 0, b = 0; int i = 0;
+        int xor_res = getXOR(arr), tmp_res = 0;
+        cout << xor_res << endl;
+        for (int j = arr.size()-1; j >= i; j--) {
+            if (j+1 <= arr.size()-1) xor_res ^= arr[j+1];
+            tmp_res = xor_res;
+            for (int i = 0; i < j; i++) {
+                if (i-1 >= 0) tmp_res ^= arr[i-1];
+                if (tmp_res == 0) c += j-i;
             }
         }
         return c;
