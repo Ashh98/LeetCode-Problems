e42947ca9b8db95b3b7bb688d60265f942c5e706
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2024-05-24 17:32:32 +0300

    Time: 0 ms (100.00%), Space: 7.8 MB (17.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,11 +3,8 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> mx(m+2, vector<int>(n+2, 0));
         mx[1][1] = 1;
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= m; i++) for (int j = 1; j <= n; j++) 
                 if (!(i == 1 && j == 1)) mx[i][j] = mx[i-1][j] + mx[i][j-1];
-            }
-        }
         
         return mx[m][n];
     }
