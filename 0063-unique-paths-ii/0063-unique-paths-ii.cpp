c625fcf8a852008141ce6f7621eb9517184ccd83
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2024-06-02 21:13:33 +0300

    Time: 0 ms (100.00%), Space: 10.4 MB (8.07%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
+        vector<vector<long long>> dp(obstacleGrid.size()+2, vector<long long>(obstacleGrid[0].size()+2, 0));
+        dp[1][1]=2;
+        
+        for (int i = 1; i < dp.size()-1; i++) {
+            for (int j = 1; j < dp[0].size()-1; j++) {
+                if (i == 1 && j == 1) continue;
+                dp[i][j] = obstacleGrid[i-1][j-1];
+            }
+        }
+        
+        for (int i = 1; i < dp.size()-1; i++) {
+            for (int j = 1; j < dp[0].size()-1; j++) {
+                if (dp[i][j] == 1) {
+                    continue;
+                } else if (dp[i-1][j] == 1) {
+                    if (dp[i][j-1] != 1) dp[i][j]+=dp[i][j-1];
+                } else if (dp[i][j-1] == 1) {
+                    if (dp[i-1][j] != 1) dp[i][j]+=dp[i-1][j];
+                } else {
+                    dp[i][j]+=(dp[i-1][j]+dp[i][j-1]);
+                }
+            }
+        }
+     
+        return dp[dp.size()-2][dp[0].size()-2]/2;
+    }
+};
