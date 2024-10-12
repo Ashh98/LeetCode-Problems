9f3012984746cf537d127cfa857cc8a6ab0fc3d1
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2024-10-12 07:45:53 +0300

    Time: 358 ms (5.00%), Space: 66.8 MB (7.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,22 +3,24 @@ public:
     unordered_map<int, int> memo;
     
     int coinChange(vector<int>& coins, int amount) {
-        if (amount == 0) return 0;
-        if (amount < 0) return -1;
-        
-        if (memo.find(amount) != memo.end()) {
-            return memo[amount];
-        }
+        if (amount == 0)
+            return 0;
+        else if (amount < 0)
+            return -1;
         
         int answer = INT_MAX;
         
+        if (memo.find(amount) != memo.end())
+            return memo[amount];
+        
         for (auto& coin : coins) {
-            int subproblem = coinChange(coins, amount - coin);
-            if (subproblem >= 0) 
-                answer = min(answer, subproblem + 1);
+            int subproblem = coinChange(coins, amount-coin);
+            if (subproblem >= 0)
+                answer = min(answer, subproblem+1);
         }
         
-        memo[amount] = (answer == INT_MAX) ? -1 : answer;
+        memo[amount] = answer == INT_MAX ? -1 : answer;
+        
         return memo[amount];
     }
 };
